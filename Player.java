a2d6cfb5e3a56077c4a89f99ac58f88383e8bb2f
Matthew Pineda <mpineda3247@gmail.com>
2018-05-27 22:57:54 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,48 +1,47 @@
-// Class Player for CSCI 145 Project 2 Spring 18
-// Modified by:Subham Thakulla Kshetri
-
 import java.util.*;
 
-//************************************************************************
-//   Class Player represents one roulette player.
-//************************************************************************
 class Player {
-    private static final int RELOAD_AMOUNT = 50;
-    private int bet, money, betType, number, record, initalMoney, Housemoney;
-    private int count = 0;
-    private String name;
 
-    // =====================================================================
-    // The Player constructor sets up name and initial available money.
-    // =====================================================================
-    public Player(String playerName, int initialMoney) {
+    public int bet, money, betType, number, record, initalMoney, Housemoney, IDno, playerType;
+    public int count = 0;
+    public String name = "0";
+
+    public Player(int number, int ID, int initialMon, String playerName) {
         name = playerName;
-        money = initialMoney;
-        this.initalMoney = initialMoney;
+        money = initialMon;
+        initalMoney = initialMon;
         record = money;
-    } // constructor Player
+        IDno = ID;
+        playerType = number;
+    }
 
-    // =====================================================================
-    // Returns this player's name.
-    // =====================================================================
+    public Player(int initialMoney) {
+
+        this.initalMoney = initialMoney;
+        playerType = 0;
+
+    }
+
     public String getName() {
         return name;
-    } // method getName
+    }
 
-    // =====================================================================
-    // Returns this player's current available money.
-    // =====================================================================
+    public int getBet() {
+        return bet;
+    }
+
     public int getMoney() {
         if (money == 0) {
-            money = RELOAD_AMOUNT;
-            count++;
+            System.out.println("Sorry you don't have any money!");
+            // Need to remove this player from the queue
         }
         return money;
-    } // method getMoney
+    }
 
-    // =====================================================================
-    // Prompts the user and reads betting information.
-    // =====================================================================
+    public int playerType() {
+        return playerType;
+    }
+
     public void makeBet(Scanner scan) {
 
         boolean a = true;
@@ -55,15 +54,17 @@ class Player {
                 System.out.print("Invalid Input! Please enter bet within your total amount: ");
                 bet = scan.nextInt();
                 a = true;
-            } else if (bet >= Wheel.MIN_BET && bet <= Wheel.MAX_BET) {
-                a = false;
-            } else {
-                System.out.print("Invalid Input! Please enter valid Bet($1-$10): ");
-                bet = scan.nextInt();
+            } else if (bet < Game.minBet && bet > Game.maxBet)// Need to instantiate MIN and Max
+            //  i.e. wheel no longer contains those
+            {	System.out.println("Invalid Bet amount!!");
+                System.out.println("Please enter amount between"+Game.minBet+" to "+ Game.maxBet);
                 a = true;
+            } else {
+
+                a = false;
             }
         }
-        money-=bet;
+        money -= bet;
         Housemoney += bet;
         Wheel.betOptions();
         System.out.print("Enter the number to command for bet:");
@@ -75,42 +76,48 @@ class Player {
         if (betType == Wheel.NUMBER) {
             System.out.print("Enter the number to bet on:");
             number = scan.nextInt();
-            while (number < 1 || number > 14) {
+            while (number >= Wheel.MIN_NUM || number <= Wheel.MAX_POSITIONS) {
                 System.out.print("Invalid Input. Please enter number between 1 to 14:");
                 number = scan.nextInt();
             }
         }
 
-    } // method makeBet
 
-    // =====================================================================
-    // Determines if the player wants to play again.
-    // =====================================================================
-    public boolean playAgain(Scanner scan) {
-        String answer;
-        System.out.print("Play again [y/n]? ");
-        answer=scan.next();
-        return answer.equalsIgnoreCase("y");
+
     }
 
+/*	public boolean playAgain(Scanner scan) {
+		String answer;
+		System.out.print("Play again [y/n]? ");
+		answer = scan.next();
+		return answer.equalsIgnoreCase("y");
+	}*/ 													//Moving to the game class
+
     public void payment() {
-        money = money + Wheel.payOff(bet, betType, number);
-        System.out.println(name + " won: " + Wheel.payOff(bet, betType, number));
-        if (Wheel.payOff(bet, betType, number) == 0) {
+        money = money + Wheel.payoff(bet, betType, number);
+        System.out.println(name + " won: " + Wheel.payoff(bet, betType, number));
+        if (Wheel.payoff(bet, betType, number) == 0) {
             System.out.println("New Balance: " + (money));
 
         } else {
             money += bet;
             System.out.println("New Balance: " + (money));
         }
-        Housemoney -= Wheel.payOff(bet, betType, number);
+        Housemoney -= Wheel.payoff(bet, betType, number);
     }
 
-    // =====================================================================
-    // Return relevant information about the player.
-    // =====================================================================
+
+    public void addTransaction() {
+        Game.transactionLines.add(new Transaction( getName(), getBet(), Wheel.payoutAmount ));
+
+    }
+
+    public void addsubMoney(int net) {
+        money += net;
+    }
+
     public String toString() {
-        String result;
+        String result = "";
         if (count <= 0) {
             int result1 = money - record;
             System.out.println();
@@ -124,22 +131,10 @@ class Player {
                 System.out.println("---------Better Luck next time!!--------- " + name);
                 result = "Total money lost by " + name + " is $" + (result1 + count * initalMoney);
             }
-        } else {
-            int result1 = money - RELOAD_AMOUNT;
-            if (result1 > 0) {
-                System.out.println("----------Congratulations!--------");
-                System.out.println(name + ", You won..");
-                result = "Total money won by " + name + " is $" + (result1 - count * initalMoney);
-            } else if (result1 == 0) {
-                result = name + " did not win nor did lose..";
-            } else {
-                result1 *= -1;
-                System.out.println("---------Better Luck next time!!------- " + name);
-                result = "Total money lost by " + name + " is $" + (result1 + count * initalMoney);
-            }
         }
         System.out.println(" Housemoney because of " + name + ":$" + Housemoney);
         System.out.println();
         return result;
     } // method toString
 }
+
