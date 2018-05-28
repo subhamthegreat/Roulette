244ff6755d9351dd75d6b46f8e984e84e3f24a3e
Matthew Pineda <mpineda3247@gmail.com>
2018-05-27 23:00:30 -0700

    Update Wallet.java

--- Wallet.java
+++ Wallet.java
@@ -1,4 +1,37 @@
-public class Wallet {
-
-
-}
+//public class Wallet {
+//
+//    public Player[] players = new Player[25];
+//
+//    public int[] houseWinLosses = new int[5];
+//
+//    public int i = 0;
+//
+//    public Wallet(int house, int money){
+//
+//
+//        players[i] = ;
+//        houseWinLosses[i] = house;
+//        i++;
+//
+//    }
+//
+//    public String walletSituation(int game) {
+//
+//        return "Game " + game + " Net Money: " + players[game]
+//                + " House Net Money: " + houseWinLosses[game];
+//
+//    }
+//
+//    public int getHouseWinLosses(int game) {
+//        return houseWinLosses[game];
+//    }
+//
+//
+//    public int getplayers(int game) {
+//        return players[game];
+//    }
+//
+//    public void setWallet(int house, int money) {
+//
+//    }
+//}
