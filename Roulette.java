bddc522a6458bae091553f601aaf245c42bd3b73
Matthew Pineda <mpineda3247@gmail.com>
2018-05-27 22:58:26 -0700

    Update Roulette.java

--- Roulette.java
+++ Roulette.java
@@ -10,43 +10,57 @@ class Roulette {
     // =====================================================================
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Player player1 = new Player("Jane", 60); // $60 to start for Jane
-        Player player2 = new Player("Mark", 60); // getting another player into the game
-        boolean done = false, done1 = false;
+//        Player player1 = new Player("Jane", 60); // $60 to start for Jane
+//        Player player2 = new Player("Mark", 60); // getting another player into the game
 
-        System.out.println("<<<Author: Subham Thakulla Kshetri>>>");
+//        boolean done = false, done1 = false;
+
+        System.out.println("Authors: ");
+        System.out.println("     Subham Thakulla Kshetri");
         System.out.println("     Matthew Pinada");
         System.out.println("     Jose Sanchez");
-        Wheel.welcomeMessage();
+        Dis.display3();
 
-        while (!done || !done1) {
-            if (done == false) {
-                System.out.println("Money available for " + player1.getName() + ": " + player1.getMoney());
-                player1.makeBet(scan);
-                System.out.println();
-            }
-            if (done1 == false) {
-                System.out.println("Money available for " + player2.getName() + ": " + player2.getMoney());
-                player2.makeBet(scan);
-                System.out.println();
-            }
-            Wheel.Spin();
-            if (done == false) {
-                player1.payment();
-                done = !player1.playAgain(scan);
-            }
-            if (done1 == false) {
-                player2.payment();
-                done1 = !player2.playAgain(scan);
-            }
+
+        for (int i = 0; i < Game.playersInGame.size(); i++) {
+
+            System.out.println( "Money available for " + Game.playersInGame.get(i).getName() + ": " + Game.playersInGame.get(i).getMoney());
+
+
+
+        }
+
+//        while (!done || !done1) {
+//            if (done == false) {
+//                System.out.println("Money available for " + player1.getName() + ": " + player1.getMoney());
+//                player1.makeBet(scan);
+//                System.out.println();
+//            }
+//            if (done1 == false) {
+//                System.out.println("Money available for " + player2.getName() + ": " + player2.getMoney());
+//                player2.makeBet(scan);
+//                System.out.println();
+//            }
+//            Wheel.spin();
+//            if (done == false) {
+//                player1.payment();
+//                done = !player1.playAgain(scan);
+//            }
+//            if (done1 == false) {
+//                player2.payment();
+//                done1 = !player2.playAgain(scan);
+//            }
 
             System.out.println();
 
+
+//        System.out.println(player1);
+//        System.out.println(player2);
+//        System.out.println();
+//        System.out.println("------------Game over!  Thanks for playing.---------------");
+//        scan.close();
         }
-        System.out.println(player1);
-        System.out.println(player2);
-        System.out.println();
-        System.out.println("------------Game over!  Thanks for playing.---------------");
-        scan.close();
+
+
     }
-}
+
