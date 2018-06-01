e01dabfc123f984c84822beb25f10c202cb41795
Matthew Pineda <mpineda3247@gmail.com>
2018-05-31 18:26:29 -0700

    Add files via upload

---
+++ Situation.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class Situation {
+
+//    public static void main(String args[]) throws IOException {
+//
+//        String str = "Hello";
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("AllGameResults"));
+//        writer.write(str);
+//
+//        writer.close();
+//
+//    }
+
+
+
+
+}
